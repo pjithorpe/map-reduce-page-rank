40c651b2816913650062343302b67bdfd588488e
Connor Bazely <2199728b@bo1028-1-01u.dcs.gla.ac.uk>
2019-02-19 22:27:31 +0000

    hey good job fixing that buddy
    newline

--- uog-bigdata/src/main/java/mapreduce/MyLoopingMapReduceJob.java
+++ uog-bigdata/src/main/java/mapreduce/MyLoopingMapReduceJob.java
@@ -1,10 +1,14 @@
 package mapreduce;
 
 import java.io.IOException;
+import java.util.Arrays;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -15,7 +19,7 @@ import org.apache.hadoop.util.ToolRunner;
 public class MyLoopingMapReduceJob extends Configured implements Tool {
 
 	// Your mapper class; remember to set the input and output key/value class appropriately in the <...> part below.
-	static class MyMapper extends Mapper<IntWritable, Text, IntWritable, Text> {
+	static class MyMapper extends Mapper<Text, Text, Text, Text> {
 		@Override
 		protected void setup(Context context) throws IOException, InterruptedException {
 			super.setup(context);
@@ -23,32 +27,78 @@ public class MyLoopingMapReduceJob extends Configured implements Tool {
 		}
 
 		// The main map() function; the input key/value classes must match the first two above, and the key/value classes in your emit() statement must match the latter two above.
+		Text textKey = new Text();
+		Text textValue = new Text();
+		FloatWritable floatWritable = new FloatWritable();
+		
+		/** extended from example at "https://coe4bd.github.io/HadoopHowTo/stringMultipleValues/stringMultipleValues.html"
+		* An example of how to pass multiple values from a mapper to a reducer in a
+		* single string value via string concatenation.
+		*/
+		
 		@Override
-		protected void map(IntWritable key, Text value, Context context) throws IOException, InterruptedException {
-			// ...
+		public void map(Text key, Text value, Context context) throws IOException, InterruptedException{
+		    String line = value.toString();
+		    String[] field = line.split(" ");
+		    float pr = Float.parseFloat(field[-1]);
+		    float prFraction = pr/field.length;
+		    field = Arrays.copyOf(field, field.length-1);
+		    StringBuilder original = new StringBuilder("<");
+		    original.append(key.toString());
+			original.append(" ");
+		    original.append(Arrays.toString(field));
+		    original.append(">");
+			String originalFinal = original.toString();
+			textKey.set(key);
+		    textValue.set(originalFinal);
+		    context.write(textKey, textValue);
+		    textValue.set(String.valueOf(prFraction));
+		    
+		    for (String outlink : field) {
+		    	textKey.set(outlink);
+		    	context.write(textKey, textValue);
+			  }
+		    }
 		}
-
-		@Override
-		protected void cleanup(Context context) throws IOException, InterruptedException {
-			// ...
-			super.cleanup(context);
-		}
-	}
+	
 
 	// Your reducer class; remember to set the input and output key/value class appropriately in the <...> part below.
-	static class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	static class MyReducer<Key> extends Reducer<Key, Text , Key, Text > {
 		@Override
 		protected void setup(Context context) throws IOException, InterruptedException {
 			super.setup(context);
 			// ...
 		}
 		
+
+		Text textValue = new Text();
+		
 		// The main reduce() function; the input key/value classes must match the first two above, and the key/value classes in your emit() statement must match the latter two above.
 		// Make sure that the output key/value classes also match those set in your job's configuration (see below).
 		@Override
-		protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-			// ...
-		}
+		protected void reduce(Key key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+			  //String keyStr = key.toString();			  
+			  StringBuilder full = new StringBuilder();
+			  for (Text val : values) {
+			    full.append(val);
+			  }
+			  String full2 = full.toString();
+			  String original = StringUtils.substringBetween(full2, "<", ">");
+			  full2.replaceAll("#\\<.*?>", "");		
+			  //Text textKey = new Text();
+			  String[] out = original.split(" ", 2);
+			  String[] prs = full2.split(" ");
+			  float pr = 0.0f;
+			  for (String val : prs) {
+				  pr = pr + Integer.parseInt(val);
+			  }
+			  pr = (pr * 0.85f) + 0.15f;
+			  textValue.set(out[1] + " " + pr);
+			  context.write(key, textValue);
+			}
+			 
+		
+		
 		
 		@Override
 		protected void cleanup(Context context) throws IOException, InterruptedException {
